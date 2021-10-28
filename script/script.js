c2089e8a92bb99df82500baccb2b48aa65587b66
Caster101 <moltwobble007@gmail.com>
2021-10-28 20:02:05 +0500

    form complete

--- script/script.js
+++ script/script.js
@@ -3,12 +3,28 @@ $(document).ready(function(){
         $(".menu").fadeToggle();
         $('body').toggleClass('scroll_hidden');
     })
+    $(".slide_toggle").on('click',function(e){
+        e.preventDefault()
+        $(this).css('display','none');
+        $(".slide_none").fadeToggle('slow');
+    })
 })
 
 
-angular.module('muslim-app', []).contoller('muslim-cover', function($scope){
+angular.module('muslim-app', []).controller('muslim-cover', function($scope){
     $scope.currentTab = 0;
     $scope.nextSlide = function(){
-        $scope.currentTab += 1;
+        ($scope.currentTab >= 7 )? $scope.currentTab = 7 : $scope.currentTab += 1;
     }
+    $scope.previousSlide = function(){
+        ($scope.currentTab <= 0 )? $scope.currentTab = 0 : $scope.currentTab -= 1;
+    }
+    $scope.currentTab2 = 0;
+    $scope.nextSlide2 = function(){
+        ($scope.currentTab2 >= 9 )? $scope.currentTab2 = 9 : $scope.currentTab2 += 1;
+    }
+    $scope.previousSlide2 = function(){
+        ($scope.currentTab2 <= 0 )? $scope.currentTab2 = 0 : $scope.currentTab2 -= 1;
+    }
+
 });
